c57931927c7487219b0ad336702227ec1a9f644a
skfiroz <skfiroz5305@gmail.com>
2023-10-21 22:04:14 +0800

    question display problem fixed

--- index.js
+++ index.js
@@ -46,28 +46,30 @@ startQuiz.addEventListener("click", () => {
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
-  const data = await res.json;
+  const data = await res.json();
   quizData = data;
-  displayQuiz(data);
+  // console.log(quizData)
+  displayQuiz(quizData);
 };
 
 // Displaying quiz on quiz page
-const displayQuiz = (data) => {
-  if (!data) {
+const displayQuiz = (quizData) => {
+  console.log(quizData)
+  if (!quizData) {
     quizContainer.innerHTML = "";
     return;
   }
 
-  data.forEach((quiz, i) => {
+  quizData.forEach((Data, i) => {
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
       ${i + 1}
     </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
+    <p class="text-gray-800 text-sm">${Data.question}</p>
   </div>
   <div class="grid grid-cols-2 gap-4 mt-5">
-    ${displayQuizOptions(quiz.options, i)}
+    ${displayQuizOptions(Data.options, i)}
   </div>
 </div>`;
   });
